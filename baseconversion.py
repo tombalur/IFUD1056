37eec9872d277928881685f4f35e3c6bb3ddb3bc
Tom Are Tørum <tat@igelkott.net>
2018-02-12 11:00:24 +0100

    Commit baseconversion.py

--- baseconversion.py
+++ baseconversion.py
@@ -1,27 +1,62 @@
+import time
+
+
 def convertfrombase10_1(number, base):
     power = 0
     result = ''
     # Finding the largest possible power
-    while (number > base ** power):
-        if (number < base ** (power + 1)):
+    while number > base ** power:
+        if number < base ** (power + 1):
             break
         power += 1
-    while (power >= 0):
+    # Floor dividing the number with the highest posible power. Subtracting the ...
+    while power >= 0:
         divisionresult = number // base ** power
         number = number - (base ** power * divisionresult)
         result += str(divisionresult)
         power -= 1
-    print(result)
-    return result
+    return int(result)
+
 
 def convertfrombase10_2(number, base, result=''):
     remainder = number % base
     result = str(remainder) + result
     number = number // base
-    if (number == 0):
-        print(result)
-        return result
+    if number == 0:
+        return int(result)
     else:
-        convertfrombase10_2(number, base, result)
+        return convertfrombase10_2(number, base, result)
 
-convertfrombase10_2(9, 2)
+
+def converttobase10(number, base):
+    numbersplit = tuple(str(number))
+    i = len(numbersplit) - 1
+    result = 0
+    for digit in numbersplit:
+        result = result + (base ** i * int(digit))
+        i -= 1
+    return result
+
+
+base = 5
+number = 1455654
+starttime = time.time()
+result = convertfrombase10_1(number, base)
+endtime = time.time()
+print('Solution 1, convert from base10:')
+print('Convert base10 number ' + str(number) + ' to base' + str(base) + ' result: ' + str(result))
+print('Execution time ' + str((endtime-starttime) * 1000) + ' ms.\n')
+starttime = time.time()
+result = convertfrombase10_2(number, base)
+endtime = time.time()
+print('Solution 2, convert from base10:')
+print('Convert base10 number ' + str(number) + ' to base' + str(base) + ' result: ' + str(result))
+print('Execution time ' + str((endtime-starttime) * 1000) + ' ms.\n')
+starttime = time.time()
+number = 333040104
+result = converttobase10(number, base)
+endtime = time.time()
+print('Solution 3, convert to base10:')
+print('Convert base' + str(base) + ' number ' + str(number) + ' to base10 result: ' + str(result))
+print('Execution time ' + str((endtime-starttime) * 1000) + ' ms.')
+
