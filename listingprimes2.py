70be657882d7c4d2b06522ae2a83c3f6b61a3a65
Tom Are Tørum <tat@igelkott.net>
2018-02-16 18:17:36 +0100

    Updating with fermat

--- listingprimes2.py
+++ listingprimes2.py
@@ -1,25 +1,40 @@
-
 def isprime(number):
-    if number < 2:
+    if number <= 1:
         return False
-    elif number == 2:
+    elif number <= 3:
         return True
-    elif number == 3:
+    elif number % 2 == 0 or number % 3 == 0:
+        return False
+    counter = 5
+    while counter ** 2 <= number:
+        if number % counter == 0 or number % (counter + 2) == 0:
+            return False
+        counter += 1
+    return True
+
+
+def isprimefermat(number):
+    randomnumber = 5
+    if number <= 1:
+        return False
+    elif number <= 3:
         return True
-    elif number % 2 != 0 and number % 3 != 0:
+    elif number % 2 == 0 or number % 3 == 0:
+        return False
+    elif randomnumber ** number % number == 1:
         return True
     return False
 
 
 def findprimes(number):
     primesfound = ()
-    counter = 2
+    counter = 0
     while len(primesfound) < number:
-        if isprime(counter):
+        if isprimefermat(counter):
             primesfound += (counter,)
         counter += 1
     return primesfound
 
 
-primesfound = findprimes(1000)
-print(primesfound)
+#primesfound = findprimes(5)
+#print(primesfound)
